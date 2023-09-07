ac78bd11981d8bada2c1354b6fe14a07e6cbff9e
syedfaheem27 <syedfaheem2719@gmail.com>
2023-09-08 01:18:43 +0530

    Add functionality to change the priority of the order using the fetcher.Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,44 @@
-import Home from "./ui/Home";
-import Error from "./ui/Error";
+import Home from './ui/Home';
+import Error from './ui/Error';
 
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import AppLayout from "./ui/AppLayout";
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AppLayout from './ui/AppLayout';
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         errorElement: <Error />,
         loader: menuLoader,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
-        loader: orderLoader,
         errorElement: <Error />,
+        loader: orderLoader,
+        action: updateOrderAction,
       },
     ],
   },
